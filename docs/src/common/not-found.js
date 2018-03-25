4a8f85bce39d6435327b64f8fddf7fdbf53ad091
Jmeas <jellyes2@gmail.com>
2018-03-25 12:31:01 -0700

    Update spacing of headers

--- docs/src/common/not-found.js
+++ docs/src/common/not-found.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 export default class NotFound extends Component {
   render() {
     return (
-      <div>
+      <Fragment>
         <h1 className="primaryHeader">Not Found</h1>
         <p className="paragraph">This page could not be found</p>
-      </div>
+      </Fragment>
     );
   }
 }
