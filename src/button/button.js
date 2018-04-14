a0efa3c83911314859fb6ce56a568f42f940efae
Jmeas <jellyes2@gmail.com>
2018-04-13 17:40:11 -0700

    Remove most components (for now)

--- src/button/button.js
+++ src/button/button.js
@@ -15,6 +15,7 @@ class Button extends Component {
       ripple = true,
       ...props
     } = this.props;
+
     const componentClassNames = classnames('mt-button', className, {
       'mt-button-raised': raised,
       'mt-button-flat': flat,
@@ -22,6 +23,7 @@ class Button extends Component {
       'mt-button-compact': compact,
       'mt-button-secondary': secondary
     });
+
     return (
       <button className={componentClassNames} {...props}>
         {ripple && <Ripple />}
