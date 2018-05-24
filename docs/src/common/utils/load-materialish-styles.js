9f3d5f8ea34d600b092861990b655c55b4ea1971
James Smith <jamesplease2@gmail.com>
2018-05-24 08:30:37 -0700

    Update docs to Materialish 0.4.0

--- docs/src/common/utils/load-materialish-styles.js
+++ docs/src/common/utils/load-materialish-styles.js
@@ -1,3 +1,4 @@
+import '../../styles/npm-package-styles/avatar.css';
 import '../../styles/npm-package-styles/button.css';
 import '../../styles/npm-package-styles/ripple.css';
 import '../../styles/npm-package-styles/checkbox.css';
