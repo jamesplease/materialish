fb18c979d5643b9115e2ea5879296acaebc43b4d
Jmeas <jellyes2@gmail.com>
2018-05-23 21:15:28 -0700

    Add favicon; rename txt file examples to be js files

--- docs/examples/index.js
+++
@@ -1,5 +0,0 @@
-import button from './button.txt';
-import checkbox from './checkbox.txt';
-import Switch from './switch.txt';
-
-export { button, checkbox, Switch };
--- docs/readmes/index.js
+++
@@ -1,5 +0,0 @@
-import button from './button.md';
-import checkbox from './checkbox.md';
-import Switch from './switch.md';
-
-export { button, checkbox, Switch };
