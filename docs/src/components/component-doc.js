964c9f9925b6de0de8a6b464020f6f52d94f2fc2
Jmeas <jellyes2@gmail.com>
2018-06-14 00:01:37 -0700

    Update Useful Links display on Components pages

--- docs/src/components/component-doc.js
+++ docs/src/components/component-doc.js
@@ -44,21 +44,6 @@ export class ComponentDoc extends Component {
     return (
       <div className="componentDoc">
         <h1 className="primaryHeader componentDoc_primaryHeader">{name}</h1>
-        <ul className="componentDoc_links">
-          <li className="componentDoc_linkItem">
-            <a href={sourceLinkToUse} className="componentDoc_linkAnchor">
-              <i className="componentDoc_icon zmdi zmdi-code" /> View Source
-            </a>
-          </li>
-          {materialDocsLink && (
-            <li className="componentDoc_linkItem">
-              <a href={materialDocsLink} className="componentDoc_linkAnchor">
-                <i className="componentDoc_icon zmdi zmdi-file-text" /> Material
-                Docs
-              </a>
-            </li>
-          )}
-        </ul>
         <p className="paragraph">{description}</p>
         <CodeManager code={example}>
           {({ code, handleCodeChange }) => (
@@ -90,6 +75,22 @@ export class ComponentDoc extends Component {
             </Fragment>
           )}
         </CodeManager>
+        <h2 className="secondaryHeader">Useful Links</h2>
+        <ul className="componentDoc_links">
+          <li className="componentDoc_linkItem">
+            <a href={sourceLinkToUse} className="componentDoc_linkAnchor">
+              <i className="componentDoc_icon zmdi zmdi-code" />Source Code
+            </a>
+          </li>
+          {materialDocsLink && (
+            <li className="componentDoc_linkItem">
+              <a href={materialDocsLink} className="componentDoc_linkAnchor">
+                <i className="componentDoc_icon zmdi zmdi-file-text" />Material
+                Guidelines
+              </a>
+            </li>
+          )}
+        </ul>
         <Markdown markdownText={markdown} />
       </div>
     );
