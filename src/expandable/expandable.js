9da4a268aac2388aab8d1a9d146d9d6212755fc4
Jmeas <jellyes2@gmail.com>
2018-06-23 10:50:35 -0700

    Queue Expandable

--- src/expandable/expandable.js
+++ src/expandable/expandable.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 
 export default class Expandable extends Component {
   render() {
-    const { className = '', open = false, durationMs, ...props } = this.props;
+    const { className = '', durationMs, ...props } = this.props;
+    const { isOpen } = this.state;
 
     return (
       <div
         ref={this.getRef}
         className={`mt-expandable ${
-          open ? 'mt-expandable-open' : ''
+          isOpen ? 'mt-expandable-open' : ''
         } ${className}`}
         {...props}
       />
@@ -19,9 +20,38 @@ export default class Expandable extends Component {
     return this.el ? this.el.getBoundingClientRect() : null;
   }
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: props.open,
+      isOpenQueue: null,
+      isAnimating: false,
+    };
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    // When the open prop differs from our previous state, then we either need to
+    // start animating, or we need to queue the open
+    if (nextProps.open !== prevState.isOpen) {
+      if (prevState.isAnimating) {
+        return {
+          isOpenQueue: nextProps.open,
+        };
+      } else {
+        return {
+          isAnimating: true,
+          isOpen: nextProps.open,
+        };
+      }
+    }
+
+    return null;
+  }
+
   componentDidUpdate(prevProps, prevState, snapshot) {
     if (snapshot && this.el) {
-      if (prevProps.open !== this.props.open) {
+      if (prevState.isOpen !== this.state.isOpen) {
         const { durationMs = 200 } = this.props;
 
         const bb = this.el.getBoundingClientRect();
@@ -31,8 +61,13 @@ export default class Expandable extends Component {
           requestAnimationFrame(() => {
             this.el.style.transition = `all ${durationMs}ms ease-out`;
             this.el.style.height = `${bb.height}px`;
+
             setTimeout(() => {
               this.el.style = '';
+
+              this.setState({
+                isAnimating: false,
+              });
             }, durationMs);
           });
         });
