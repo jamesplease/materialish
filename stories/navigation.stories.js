cc3db12f8dbc84e1c3e7e0984c71bc3859ee0e61
jporrero <porrero.javier@gmail.com>
2018-06-26 22:27:13 -0700

    navigation component and stories

---
+++ stories/navigation.stories.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, boolean } from '@storybook/addon-knobs/react';
+import { setOptions } from '@storybook/addon-options';
+import '../src/navigation/navigation.css';
+import { Navigation } from '../src/index';
+import IconAlarmOn from '../icon-alarm-on';
+import IconBugReport from '../icon-bug-report';
+import IconFace from '../icon-face';
+
+setOptions({
+  name: 'Materialish',
+  addonPanelInRight: true,
+});
+
+storiesOf('Navigation', module)
+  .addDecorator(withKnobs)
+  .add('Regular', () => (
+    <Navigation vertical={boolean('Vertical', false, 'PROPS')}>
+      <Navigation.Item active>One</Navigation.Item>
+      <Navigation.Item>Item Two Hundred and a half</Navigation.Item>
+      <Navigation.Item>Three</Navigation.Item>
+    </Navigation>
+  ))
+  .add('With Icons', () => (
+    <Navigation centered>
+      <Navigation.Item>
+        <IconAlarmOn />
+      </Navigation.Item>
+      <Navigation.Item active>
+        <IconBugReport />
+      </Navigation.Item>
+      <Navigation.Item>
+        <IconFace />
+      </Navigation.Item>
+    </Navigation>
+  ));
