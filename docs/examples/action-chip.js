3a409e07e79cd035e7be56631f9b7a0f2083105a
Jmeas <jellyes2@gmail.com>
2018-08-19 13:22:20 -0700

    Unify Chips icons API

--- docs/examples/action-chip.js
+++ docs/examples/action-chip.js
@@ -10,16 +10,12 @@ class ActionChipExample extends Component {
           Perform Action
         </ActionChip>
         {' '}
-        <ActionChip onClick={onClick}>
-          <IconDelete style={iconStyle} /> Delete Item
+        <ActionChip onClick={onClick} icon={<IconDelete/>}>
+          Delete Item
         </ActionChip>
       </Fragment>
     );
   }
 }
-
-// Note: you are responsible for adding horizontal padding to the icon
-// within the chip. Inline styles are used here as an example.
-var iconStyle = { marginRight: '0.25rem' };
 
 return <ActionChipExample />;
